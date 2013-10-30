2d3bae9c48b4e92705cdbf566df8788db104fb28
Alexandre Baron <alexandre.baron@epitech.eu>
2013-10-30 16:03:10 -0400

    * Download a file works, range download is possible by modifying the source code, and works

--- ubuntuone/login_script.py
+++ ubuntuone/login_script.py
@@ -15,6 +15,7 @@ class AuthenticationWarning(Exception):
 
 class U1Driver:
   BASE_API_PATH = "https://one.ubuntu.com/api/file_storage/v1"
+  BASE_FILES_PATH = "https://files.one.ubuntu.com"
   CREDS_FILE = '.credentials'
   
   def __init__(self, resource_host="https://edge.one.ubuntu.com", content_host="https://files.one.ubuntu.com"):
@@ -32,7 +33,7 @@ class U1Driver:
       request.add_header(header, value)
     return request
 
-  def make_oauth_credentials(self, jsoncreds):
+  def oauth_make_credentials(self, jsoncreds):
     """Creates the OAuth-related classes for credentials (used for every signed request)"""
     self.consumer = oauth2.Consumer(jsoncreds['consumer_key'], jsoncreds['consumer_secret'])
     self.token = oauth2.Token(jsoncreds['token'], jsoncreds['token_secret'])
@@ -52,13 +53,13 @@ class U1Driver:
       if httpe.code == 401: # Unauthorized
         raise AuthenticationFailure("Error 401: Bad email address or password")
       else:
-        raise
+        raise # Unhandled error
     credentials = json.load(response)
     return credentials
 
   def authenticate(self, credentials):
     """Makes first signed request using new OAuth credentials"""
-    self.make_oauth_credentials(credentials)
+    self.oauth_make_credentials(credentials)
     # Tell Ubuntu One about the new OAuth token.
     get_tokens_url = ('https://one.ubuntu.com/oauth/sso-finished-so-get-tokens/')
     request = self.oauth_sign_request(get_tokens_url)
@@ -71,22 +72,21 @@ class U1Driver:
 
   def credentials_from_email_and_password(self):
     self.prompt_user_credentials()
-    return self.oauth_get_access_token()    
-    
+    return self.oauth_get_access_token()
+
   def credentials_from_file(self, credsfile):
     """Extracts the OAuth credentials from file. If it fails, asks user if he wants to input his credentials.
     Attempts to store it in given file name."""
     try:
       with open(credsfile) as f:
         jsoncreds = json.loads(f.read())
-    except IOError as e:
-      # if file doesn't exist
+    except IOError as e: # open failed
       response = raw_input("Credentials file is unreadable or doesn't exist.\nWould you like to input your U1 credentials instead (will attempt storing the credentials in " + U1Driver.CREDS_FILE + ") ? [Yn] ")
       if response == '' or response.upper() == 'Y':
         jsoncreds = self.credentials_from_email_and_password()
       else:
         raise AuthenticationFailure("Cannot open credentials file (" + credsfile + ")")
-    except ValueError:
+    except ValueError: # file opened, but unable to json.loads the contents
       response = raw_input("Credentials file contents corrupted.\nWould you like to input your U1 credentials instead (will attempt storing the credentials in " + U1Driver.CREDS_FILE + ") ? [Yn] ")
       if response == '' or response.upper() == 'Y':
         jsoncreds = self.credentials_from_email_and_password()
@@ -122,10 +122,44 @@ class U1Driver:
   def logout(self):
     pass
   
-  def get(self, file_path, *args):
-    pass
+  def get(self, args, bytes_range=''):
+    """Downloads a remote file. It is a two-step process:
+    first acquire the metadatas on the API, then download the actual content of the file from the Files address."""
+    url = U1Driver.BASE_API_PATH 
+    url += '/~/' if not args[0].startswith('/~/') else ''
+    url += urllib2.quote(args[0])
+    request = self.oauth_sign_request(url)
+    print 'Sending request to', url
+    try:
+        rep = urllib2.urlopen(request)
+    except urllib2.HTTPError as httpe:
+        print "Error %i finding: %s" % (httpe.code, url)
+        return
+    metadatas = json.loads(rep.read())
+    url = U1Driver.BASE_FILES_PATH + urllib.quote(metadatas.get('content_path'), safe="/")
+    request = self.oauth_sign_request(url)
+    if bytes_range != '':
+      request.add_header('Range', 'bytes=' + bytes_range)
+    print 'Downloading contents of ', url
+    try:
+        rep = urllib2.urlopen(request)
+    except urllib2.HTTPError as httpe:
+        print "Error %i finding: %s" % (httpe.code, url)
+        return
+    content = rep.read()
+    try:
+      local_filename = args[1]
+    except IndexError:
+      local_filename = args[0].split('/')[-1]
+    print 'Writing contents to', local_filename
+    try:
+      with open(local_filename, 'wb') as local_file:
+        local_file.write(content)
+    except IOError:
+      print "Unable to write to file %s" % local_filename
+    print 'Done'
 
-  def put(self, file_path, *args):
+  def put(self, *args):
     pass
 
   def delete(self, file_path, *args):
@@ -142,20 +176,19 @@ class U1Driver:
         print "Error finding: %s" % url
         return
     basepaths = json.loads(response.read())
-    print basepaths
-    if volume == '':
+    if volume == '': # base call: list every subsequent directory (recursively)
       self.list_files(basepaths['root_node_path'])
       for user_volume in basepaths['user_node_paths']:
         self.list_files(user_volume)
-    else:
-      print ('\t'*tabs_nbr) + basepaths.get('resource_path').split('/')[-1] + '/'
+    else: # lists volume and each subdirectory recursively
+      print ('  '*tabs_nbr) + basepaths.get('resource_path').split('/')[-1] + '/'
       if basepaths.get('has_children') == True:
         for child in basepaths.get('children'):
           if child['kind'] == 'directory':
             self.list_files(child['resource_path'], tabs_nbr+1)
           elif child['kind'] == 'file':
-            print ('\t'*(tabs_nbr+1)) + child[u'path'].split('/')[-1]
-            
+            print ('  '*(tabs_nbr+1)) + child[u'path'].split('/')[-1]
+
   def quit(self):
     pass
 
@@ -171,7 +204,7 @@ class U1Driver:
       words = command.split()
       try:
         if command != '':
-          cmds[words[0]](words[1]) if words[0] != 'list' else self.list_files("/~/Ubuntu One/")
+          cmds[words[0]](words[1:]) if words[0] != 'list' else self.list_files()
       except KeyError:
         print "Unknown command:", words[0]
       except IndexError:
