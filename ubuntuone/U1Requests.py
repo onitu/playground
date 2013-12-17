c57b75ae81389f65050ca0bbbe1fbbd3b80562ed
Alexandre Baron <alexandre.baron@epitech.eu>
2013-12-17 13:48:17 -0500

    A bit of refactoring, starting the volumes listing part

--- ubuntuone/U1Requests.py
+++ ubuntuone/U1Requests.py
@@ -9,79 +9,159 @@ class RequestException(Exception):
 
 
 class U1Request:
-    def __init__(self, handler, end_msg_type, ):
-        self.req_id = handler.msg_id
-        handler.requests[handler.msg_id] = self
-        handler.msg_id += 2 # to stay odd
-        self.end_msg_type = end_msg_type
-        self.responses = []
-        self.handler = handler
-        self.trigger = self.initialize()
-
-    def assert_msg_type(self, msg, expected_type):
-        try:
-            assert msg.type == expected_type
-        except AssertionError:
-            raise RequestException("Server didn't send expected answer")
-
-    def process(self, handler):
-        raise NotImplementedError()
-    
-    def finish(self):
-        raise NotImplementedError()
-    
-    def initialize(self):
-        raise NotImplementedError()
-    
-class AuthRequest(U1Request):
-    CREDENTIALS_FILE = ".credentials" # Our OAuth credentials file
-
     def __init__(self, handler):
-        U1Request.__init__(self, handler, protocol_pb2.Message.ROOT)
+      handler.requests[handler.msg_id] = self
+      self.req_id = handler.msg_id
+      handler.msg_id += 2 # to stay odd
+      self.handler = handler # the u1 handler
+      self.responses = [] # the stored responses to the request
+      self.counter = 0 # response counter. useful to know "when" a response has been sent in the process. We cannot only rely on len(self.responses), because since some responses are useless to store, they would be deleted right away 
+      self.states = {} # A request kind of works like a state machine. Stores the different states the request can be in
+      self.message = message = protocol_pb2.Message() # create a new protobuf message
+      self.message.id = self.req_id # give it the right id
+      self.initialize() # for further message initialization
 
+    
     def initialize(self):
-        # retrieve our OAuth credentials from file
-        oauth_creds = self.credentials_from_file()
-        message = protocol_pb2.Message()
-        message.id = self.req_id
-        message.type = protocol_pb2.Message.AUTH_REQUEST
-        # Signs the message with oauth
-        client = Client(oauth_creds["consumer_key"], oauth_creds["consumer_secret"],
-                        oauth_creds["token"], oauth_creds["token_secret"],
-                        signature_method=SIGNATURE_PLAINTEXT,
-                        signature_type=SIGNATURE_TYPE_QUERY)
-        url, headers, body = client.sign('http://server')
-        # Parse out the authentication parameters from the query string.
-        auth_parameters = dict((name, value) for name, value in
-                               parse_qsl(urlparse(url).query)
-                               if name.startswith('oauth_'))
-        # add the authentication informations to the protobuf
-        for key, value in auth_parameters.items():
-            newparam = message.auth_parameters.add() # create a new parameter
-            newparam.name = key
-            newparam.value = value
-        return message
+      """Implementing this is up to the inheriting request class"""
+      raise NotImplementedError()
+
 
     def process(self, msg):
-        self.responses.append(msg)
-        if msg.type == protocol_pb2.Message.ERROR:
-            pass # a server error occurred
-        else:
-            if len(self.responses) == 1:
-                self.assert_msg_type(msg, protocol_pb2.Message.AUTH_AUTHENTICATED)
-                print 'Authentication OK'
-            elif len(self.responses) == 2:
-                self.assert_msg_type(msg, protocol_pb2.Message.ROOT)
-                self.finish()
+      """Method called when the server sent a response to our request.
+      What to do is up to the inheriting request class."""
+      self.responses.append(msg)
+      self.counter += 1
+      if msg.type == protocol_pb2.Message.ERROR:
+        pass # TODO: a server error occurred, what do we do ?
+      else:
+        try:
+          self.states[msg.type](msg) # call
+          if msg.type == self.end_msg_type:
+            self.finish()
+        except KeyError:
+          pass # TODO: the server sent an unexpected response to our request. what do we do ?
+        except RequestException:
+          pass # TODO: something went wrong when dealing with the response. what do we do ?
 
+    
     def finish(self):
-        root = self.responses[1].root
-        print 'ROOT: Node', root.node, 'generation:', root.generation, 'free bytes:', root.free_bytes
-        self.handler.request_finished(self.req_id)
+      """What we do upon a request is finished"""
+      self.handler.request_finished(self.req_id) # the request deletes itself of the handler dict
 
-    def credentials_from_file(self):
-        """Extracts the OAuth credentials from file"""
-        with open(AuthRequest.CREDENTIALS_FILE) as f:
-            jsoncreds = json.loads(f.read())
+
+    def msg_type(self, msg):
+      """Returns the string representation of the message id's protobuf enum name."""
+      return protocol_pb2.Message.DESCRIPTOR.enum_types_by_name['MessageType'].values_by_number[msg.type].name
+
+
+    
+class AuthRequest(U1Request):
+  """AUTH_REQUEST request class."""
+
+  CREDENTIALS_FILE = ".credentials" # Our OAuth credentials file
+
+
+  def __init__(self, handler):
+    U1Request.__init__(self, handler)
+    self.states[protocol_pb2.Message.AUTH_AUTHENTICATED] = self.authenticated
+    self.states[protocol_pb2.Message.ROOT] = self.root
+    self.end_msg_type = protocol_pb2.Message.ROOT # the type of message signing the end of request
+
+
+  def initialize(self):
+    """Retrieves the OAuth credentials."""
+    # retrieve our OAuth credentials from file
+    def credentials_from_file():
+      """Extracts the OAuth credentials from file"""
+      with open(AuthRequest.CREDENTIALS_FILE) as f:
+        jsoncreds = json.loads(f.read())
         return jsoncreds
+
+    oauth_creds = credentials_from_file()
+    self.message.type = protocol_pb2.Message.AUTH_REQUEST
+    # Signs the message with oauth
+    client = Client(oauth_creds["consumer_key"], oauth_creds["consumer_secret"],
+                    oauth_creds["token"], oauth_creds["token_secret"],
+                    signature_method=SIGNATURE_PLAINTEXT,
+                    signature_type=SIGNATURE_TYPE_QUERY)
+    url, headers, body = client.sign('http://server')
+    # Parse out the authentication parameters from the query string.
+    auth_parameters = dict((name, value) for name, value in
+                           parse_qsl(urlparse(url).query)
+                           if name.startswith('oauth_'))
+    # add the authentication informations to the protobuf
+    for key, value in auth_parameters.items():
+      newparam = self.message.auth_parameters.add() # create a new parameter
+      newparam.name = key
+      newparam.value = value
+
+
+  def authenticated(self, msg):
+    if self.counter != 1: # To an AUTH_REQUEST request, ROOT should come second
+      raise RequestException("Server didn't send the good response in expected time (protocol error?")
+    print 'Authentication OK, session id:', msg.session_id
+    self.handler.session_id = msg.session_id # keep it for log purposes
+
+
+  def root(self, msg):
+    if self.counter != 2: # To an AUTH_REQUEST request, ROOT should come second
+      raise RequestException("Server didn't send the good response in expected time (protocol error?")
+    root = msg.root
+    print 'ROOT: Node', root.node, 'generation:', root.generation, 'free bytes:', root.free_bytes    
+
+
+class ListVolumes(U1Request):
+  """LIST_VOLUMES request class."""
+
+
+  def __init__(self, handler):
+    U1Request.__init__(self, handler)
+    self.states[protocol_pb2.Message.VOLUMES_INFO] = self.volumes_info
+    self.states[protocol_pb2.Message.VOLUMES_END] = self.volumes_end
+    self.end_msg_type = protocol_pb2.Message.VOLUMES_END # the type of message signing the end of request
+
+
+  def initialize(self):
+    """Ask the server to list the current volumes."""
+    self.message.type = protocol_pb2.Message.LIST_VOLUMES
+
+
+  def volumes_info(self, msg):
+    print 'volume info:'
+    volume_info = msg.list_volumes
+    if volume_info.type == protocol_pb2.Volumes.ROOT:
+      print 'ROOT node:', print volume_info.root.node, 'generation:', volume_info.root.generation, 'free bytes:', volume_info.root.free_bytes
+    elif volume_info.type == protocol_pb2.Volumes.UDF:
+      print 'User Defined Folder:', volume_info.udf.volume, 'node:', volume_info.udf.node, 'suggested path:', volume_info.udf.suggested_path, 'generation:', volume_info.udf.generation, 'free bytes:', volume_info.udf.free_bytes
+    elif volume_info.type == protocol_pb2.Volumes.SHARE:
+      print 'Share'
+      # Shares are a bit more complicated. Not managed yet
+
+
+  def volumes_end(self, msg):
+    print 'End of the volumes listing'
+
+
+
+# class ProtocolRequest(U1Request):
+#   """PROTOCOL_VERSION request class."""
+
+
+#   def __init__(self, handler):
+#     U1Request.__init__(self, handler)
+#     self.states[protocol_pb2.Message.PROTOCOL_VERSION] = self.protocol_version
+#     self.end_msg_type = protocol_pb2.Message.PROTOCOL_VERSION # the type of message signing the end of request
+
+
+#   def initialize(self):
+#     """Ask the server what protocol version is it running."""
+#     message = protocol_pb2.Message()
+#     message.id = self.req_id
+#     message.type = protocol_pb2.Message.PROTOCOL_VERSION
+#     self.message = message # don't forget to store the created message in self ! for it to be sent
+
+#   def protocol_version(self, msg):
+#     print 'version:', msg.protocol.version
+    
 
--- ubuntuone/tornado_test.py
+++ ubuntuone/tornado_test.py
@@ -1,26 +1,23 @@
-import socket
-import tornado
-import tornado.ioloop
-import tornado.iostream
-from tornado import gen
-
+import socket, json, struct
 from urlparse import urlparse, parse_qsl
-from oauthlib.oauth1 import Client, SIGNATURE_PLAINTEXT, SIGNATURE_TYPE_QUERY
-import json, protocol_pb2, struct
-
+import tornado, tornado.ioloop, tornado.iostream
+from tornado import gen
+import protocol_pb2
 import U1Requests
 
 class U1Handler:
-    FMT_MSG_SIZE = "!I" # In U1 protocol, peers always send the size of a message before sending the message itself. It is the format string to use to get that message size.
+    FMT_MSG_SIZE = "!I" # In U1 protocol, size of a message is sent before the message itself. It is the format string to use to get that message size
     HOST, PORT = ("fs-1.one.ubuntu.com", 443) # The U1 server we're connecting to and the port
 
     def __init__(self):
         self.volumes = []
-        # TODO load the volumes here
-        self.msg_id = 1 # we are a client, we send odd requests
-        self.requests = {} # Dict of our pending requests. Key is the msg_id
+        # TODO load the saved info about volumes here
+        self.msg_id = 1 # we are a client, we send odd requests, starting at 1
+        self.requests = {} # Dict of our pending requests. The message id is the key
+
 
     def connect(self):
+        """Start of the tornado ioloop. Base of everything"""
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setblocking(0)
         self.stream = tornado.iostream.SSLIOStream(sock) # Let Tornado handle the ssl stuff for us
@@ -28,20 +25,19 @@ class U1Handler:
         self.io_loop = tornado.ioloop.IOLoop.instance()
         self.io_loop.start()
 
-    ### Data transport & waiting methods
 
-    @gen.coroutine
     def send_request(self, req):
         """Function used to finalize a protobuf sending.
         Sets the id of the protobuf and increment the class' msg_id.
         Sets a callback in wait for the response."""
-        message = req.trigger.SerializeToString()
+        print 'sending req', req.req_id
+        message = req.message.SerializeToString()
         # send the message length (according to protocol)
         msgLength = struct.pack(U1Handler.FMT_MSG_SIZE, len(message))
         self.stream.write(msgLength)
         # send the actual message, and set a callback for the response
         self.stream.write(message, self.receive_message)
-        self.receive_message() # wait for the answer
+
 
     def process_message(self, data):
         msg = protocol_pb2.Message()
@@ -50,11 +46,14 @@ class U1Handler:
             try:
                 self.requests[msg.id].process(msg)
             except KeyError: # should not happen
-                print 'Server Error', msg.id
+                print 'Server Error', msg.id # TODO: what do we do ?
                 self.io_loop.stop()
+            except RequestException as re: # The server didn't send the expected request
+                pass # TODO: don't know what to do by now. maybe send an ERROR message ?
         else: # Even : unsolicited message from the server !
-            pass # nothing atm
+            pass # TODO: nothing atm.
         self.receive_message() # wait for a new message
+
 
     @gen.coroutine
     def receive_message(self):
@@ -63,37 +62,43 @@ class U1Handler:
         data = yield gen.Task(self.stream.read_bytes, msgLength)
         self.process_message(data)
 
+
     def request_finished(self, req_id):
+        """Function called by a request when it's finished, to delete itself of the requests dict."""
         del self.requests[req_id]
+
 
     ### Data transfer functions
     @gen.coroutine
     def initialize_connection(self):
-        greeting = yield gen.Task(self.stream.read_until, b"\r\n")
-        print greeting # We really don't care about the greeting
-        self.authenticate()
-#        self.list_volumes()
+        """First contact with the Ubuntu One server.
+        Reads the server greeting, authenticates ourselves, list the current volumes on the server,
+        then asks it a delta from the last generation we know of"""
+        greeting = yield gen.Task(self.stream.read_until, b"\r\n") # get the server greeting
+        print greeting # whatever (we don't care about the greeting)
+        self.send_request(U1Requests.AuthRequest(self)) # First mandatory step : OAuth authentication
+        self.send_request(U1Requests.ListVolumes(self)) # check the server's volumes list
+
 
     def authenticate(self):
+        """First mandatory step of the U1 connection.
+        We create a authentication request by sending our OAuth credentials.
+        The server should respond with AUTH_AUTHENTICATED and ROOT commands."""
         auth_request = U1Requests.AuthRequest(self)
         self.send_request(auth_request)
 
-    def test(self):
-        print 'test'
 
-def msg_type(msg):
-    return protocol_pb2.Message.DESCRIPTOR.enum_types_by_name['MessageType'].values_by_number[msg.type].name
+    def list_volumes(self):
+        """Ask the server about the current registered Ubuntu One volumes.
+        We need to check if there are new volumes that appeared since the last time."""
+        list_volumes
 
 handler = U1Handler()
-
-def test_entrypoint():
-    global handler
-    
-    handler.test()
 
 def start():
     global handler
     handler.connect()
-    
+
+
 if __name__ == "__main__":
     start()
