159f98b72ff46dbdf4790fdde273cbbe9c2de091
Antoine Rozo <antoine.rozo@gmail.com>
2014-05-16 12:07:12 +0200

    Escalator: Refactoring client files

--- escalator/client/escalator.py
+++ escalator/client/escalator.py
@@ -3,37 +3,7 @@ from threading import Lock
 import zmq
 
 import protocol
-
-
-class WriteBatch(object):
-    def __init__(self, db, transaction):
-        self.db = db
-        self.transaction = transaction
-        self.requests = []
-
-    def write(self):
-        self.requests.insert(0, protocol.format_request(protocol.BATCH,
-                                                        self.db.db_uid,
-                                                        self.transaction))
-        self.db.socket.send_multipart(self.requests)
-        protocol.extract_response(self.db.socket.recv())
-        self.requests = []
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, traceback):
-        if not self.transaction or not type:
-            self.write()
-
-    def _request(self, cmd, *args):
-        self.requests.append(protocol.format_request(cmd, None, *args))
-
-    def put(self, key, value):
-        self._request(protocol.PUT, key, value)
-
-    def delete(self, key):
-        self._request(protocol.DELETE, key)
+from .batch import WriteBatch
 
 
 class Escalator(object):
@@ -93,6 +63,7 @@ class Escalator(object):
 
     def write_batch(self, transaction=False):
         return WriteBatch(self, transaction)
+
 
 if __name__ == '__main__':
     from multiprocessing.pool import ThreadPool
